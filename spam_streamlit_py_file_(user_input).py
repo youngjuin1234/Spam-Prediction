d92f9f2cdc57a5601647ca5e1f25fdecf4c404ed
youngjuin1234 <75354866+youngjuin1234@users.noreply.github.com>
2024-03-26 10:43:40 +0800

    Update spam_streamlit_py_file_(user_input).py

--- spam_streamlit_py_file_(user_input).py
+++ spam_streamlit_py_file_(user_input).py
@@ -1,32 +1,32 @@
-# import streamlit as st
-# from joblib import load
-# import pandas as pd
+import streamlit as st
+from joblib import load
+import pandas as pd
 
-# # Load your logistic regression model and CountVectorizer
-# lr_loaded = load('logistic_regression_model.joblib')
-# cv_loaded = load('count_vectorizer.joblib')
+# Load your logistic regression model and CountVectorizer
+lr_loaded = load('logistic_regression_model.joblib')
+cv_loaded = load('count_vectorizer.joblib')
 
-# # Streamlit application starts here
-# def main():
-#     # Title of your web app
-#     st.title("Spam/Ham Prediction App")
+# Streamlit application starts here
+def main():
+    # Title of your web app
+    st.title("Spam/Ham Prediction App")
 
-#     # Text box for user input
-#     user_input = st.text_input("Enter a sentence to check if it's spam or ham:")
+    # Text box for user input
+    user_input = st.text_input("Enter a sentence to check if it's spam or ham:")
 
-#     # Predict button
-#     if st.button('Predict'):
-#         if user_input:  # Check if the input is not empty
-#             # Transform the user input
-#             df = pd.DataFrame([user_input], columns=['text'])
-#             Snew = cv_loaded.transform(df['text'])
+    # Predict button
+    if st.button('Predict'):
+        if user_input:  # Check if the input is not empty
+            # Transform the user input
+            df = pd.DataFrame([user_input], columns=['text'])
+            Snew = cv_loaded.transform(df['text'])
 
-#             # Make a prediction
-#             result = lr_loaded.predict(Snew)
-#             st.write(f"Predicted value: {result[0]}")
+            # Make a prediction
+            result = lr_loaded.predict(Snew)
+            st.write(f"Predicted value: {result[0]}")
 
-#         else:
-#             st.error("Please enter a sentence for prediction.")
+        else:
+            st.error("Please enter a sentence for prediction.")
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
